e95f87bdb08ea06f6b8458510acb7999d44ce098
01304814 <e1304814@student.tuwien.ac.at>
2025-01-11 13:35:01 +0100

    replace absolute by relative library path

--- src/main/java/at/ac/fhcampuswien/barcode_scanner/HelloApplication.java
+++ src/main/java/at/ac/fhcampuswien/barcode_scanner/HelloApplication.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HelloApplication extends Application {
     @Override
@@ -19,8 +21,10 @@ public class HelloApplication extends Application {
 
 
     public static void main(String[] args) {
+        Path dllPath = Paths.get("libs", "native", "opencv_java4100.dll");
+        System.load(dllPath.toAbsolutePath().toString());
 
-        System.load("C:/Users/Lil/Google Drive/FH/Programmierung_1/barcode_scanner/libs/native/opencv_java4100.dll");
+        //System.load("C:/Users/Lil/Google Drive/FH/Programmierung_1/barcode_scanner/libs/native/opencv_java4100.dll");
         launch();
     }
 }
